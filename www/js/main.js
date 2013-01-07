254682595f58416cc128b770f333533400f123d7
huangyifeng <luckyJerry85@gmail.com>
2013-01-07 18:45:54 +0800

    something about UI

---
+++ www/js/main.js
@@ -0,0 +1,13 @@
+var LEVEL_0 = {
+    LEVEL0_VERB : 0,
+    LEVEL0_ADJECTIVE : 1,
+    LEVEL0_NOUN : 2,
+    LEVEL0_OTHER : 3
+}
+
+var selectedPath = {
+	level0 : 0,
+	level1 : 0,
+	level2 : 0
+}
+
