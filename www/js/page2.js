18b4ccba058e74979ff0b7dbff7b618bedcd7bec
huangyifeng <luckyJerry85@gmail.com>
2013-01-17 18:48:34 +0800

    css part

--- www/js/page2.js
+++ www/js/page2.js
@@ -1,16 +1,29 @@
-$("#page2Page").bind("pageinit",function(event,data){
-	console.log("page2 init");
-	$("#page2ListView").empty();
-	$.get("../res/50yinRow.xml",null,function(xml){
-		$(xml).find("item").each(function(index){
-			var aItem = $("<a />",{href:"detail.html"}).html($(this).text());
-			var listItem = $("<li />",{value:index,class:"li_level2"}).append(aItem);
-			$("#page2ListView").append(listItem);
+$("#page2Page").bind(
+		"pageinit",
+		function(event, data) {
+			console.log("page2 init");
+			$("#page2ListView").empty();
+			$.get("../res/50yinRow.xml", null, function(xml) {
+				var wordType = null;
+				if (0 == selectedPath.level0) {
+					wordType = "verb";
+				}
+				var nodeName = wordType + "_type_list_" + selectedPath.level1;
+				$(xml).find("string-array[name='" + nodeName + "']").find(
+						"item").each(function(index) {
+					var aItem = $("<a />", {
+						href : "detail.html"
+					}).html($(this).text());
+					var listItem = $("<li />", {
+						value : index,
+						class : "li_level2"
+					}).append(aItem);
+					$("#page2ListView").append(listItem);
+				});
+				$(".li_level2").one("tap", function() {
+					selectedPath.level2 = $(this).attr("value");
+					console.log("page 2 tap value is %d", selectedPath.level2);
+				});
+				$("#page2ListView").listview("refresh");
+			}, "xml");
 		});
-		$(".li_level2").one("tap",function(){
-			selectedPath.level2 = $(this).attr("value");
-			console.log("page 2 tap value is %d",selectedPath.level2);
-		});
-		$("#page2ListView").listview("refresh");
-	},"xml");
-});
